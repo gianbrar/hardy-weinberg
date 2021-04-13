e33a3ff53e40f2074cb0dae110a2659b47007bd2
Gian Brar <gian.s.brar@gmail.com>
2021-04-12 23:58:58 -0400

    Update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from time import sleep
+import os
 import _thread
 import random
 import sys
@@ -17,13 +18,14 @@ from pygame.locals import (
 
 
 pygame.init()
-font = pygame.font.Font("freesansbold.ttf", 32)
+hFont = pygame.font.Font("freesansbold.ttf", 26)
+font = pygame.font.Font("freesansbold.ttf", 16)
 pygame.display.set_caption("E-VOLVE Sim")
 popCount = popCount_t = 0
 SCREEN_W = 1200
 SCREEN_H = 900
 WIN = 500 # window size
-WIN_2X = WIN/2
+WIN_2X = WIN/1.9
 WIN_2Y = WIN*1.5
 color = {"black":(0,0,0), "gray":(170,170,170), "white":(255,255,255), "red":(255,0,0), "green":(0,255,0), "blue":(0,0,255), "pink":(255,0,255), "yellow":(255,255,0), "cyan":(0,255,255),
          "bk":(0,0,0), "gy":(170,170,170), 'w':(255,255,255), 'r':(255,0,0), 'g':(0,255,0), 'b':(0,0,255), 'p':(255,0,255), 'y':(255,255,0), 'c':(0,255,255)}
@@ -35,6 +37,10 @@ t = 1
 maxGen = 0
 phenotypes = {'a':[], 'A':[]}
 q = p = 0
+dominantP = recessiveP = -1
+genlist = []
+noSelection = True
+noMutation = True
 
 def verbose(msg):
     if verboseMode:
@@ -67,9 +73,14 @@ class Pop(pygame.sprite.Sprite):
         else:
             self.gtype = list(phenotypes.keys())[random.randint(0,1)]*2
             self.gen = 0
+        genlist.append(self.gen)
         self.surf = pygame.Surface((25,25))
         self.gender = "-m" if random.randint(0,1) else "-f"
-        self.clr = tuple(map(lambda x,y : (x+y)/2, parents[0].clr, parents[1].clr)) if Color is None else Color
+        if 'A' in self.gtype: 
+            self.clr = dominantP
+        else:
+            self.clr = recessiveP
+        if self.clr == -1: self.clr = tuple(map(lambda x,y : (x+y)/2, parents[0].clr, parents[1].clr)) if Color is None else Color
         self.horny = False
         self.pregnant = False
         self.surf.fill(self.clr)
@@ -85,8 +96,10 @@ class Pop(pygame.sprite.Sprite):
         _thread.start_new_thread(self.age, ())
     def age(self):
         global t
-        while running and self.alive:
-            sleep(t*100)
+        if running and self.alive:
+            sleep(t*30)
+        self.alive = False
+        pops.remove(self)
     def move(self):
         global t
         global running
@@ -98,6 +111,12 @@ class Pop(pygame.sprite.Sprite):
             while not ((self.pos[0] > ((SCREEN_W-WIN)/2)-(WIN/4) and self.pos[0] < (SCREEN_W/2)+(WIN/4)) and (self.pos[1] > (SCREEN_H-WIN)/2 and self.pos[1] < (SCREEN_H/2)+(WIN/2))):
                 self.pos = (movement(self.cachePos[0]), movement(self.cachePos[1]))
             verbose(f">{self.Id}: Position is now {self.pos}")
+            if not random.randint(0,19):
+                verbose(f"{self.Id}: Encountered an enemy")
+                if 'A' in self.gtype:
+                    if random.randint(1,100) > (80 if 'A' in self.gtype else 25):
+                        self.alive = False
+
     def pregnate(self, father):
         global t
         global running
@@ -112,10 +131,9 @@ class Pop(pygame.sprite.Sprite):
         global pops
         global maxGen
         while (running and self.alive):
-            if self.gen+1 <= maxGen:
+            if self.gen+1 > maxGen:
                 continue
-            else:
-                sleep(t*11)
+            sleep(t*11)
             self.horny = True
             verbose(f">{self.Id}: I am horny!")
             for i in pops:
@@ -133,7 +151,9 @@ def colorize(Color):
             except KeyError:
                 error("Unknown color")
                 return -1
-            return Color
+        return Color
+
+hwe = lambda : p**2 + 2*p*q + q**2
 
 def sab(cin, t, msg):
     if len(cin) == 2:
@@ -163,6 +183,8 @@ def console():
             consoleTwo(i.split())
 def consoleTwo(cin):
     global running
+    global dominantP
+    global recessiveP
     if cin[0] == "kill":
         if "-r" in cin:
             cin.remove("-r")
@@ -191,13 +213,34 @@ def consoleTwo(cin):
                 for i in pops:
                     i.clr = Color
                     verbose(f">{i.Id}: Was painted {Color}")
-            elif ("-m" in cin) != ("-f" in cin):
+                    return
+            if "-d" in cin:
+                for i in pops:
+                    if 'A' in i.gtype:
+                        i.clr = Color
+            elif "-r" in cin:
+                for i in pops:
+                    if i.gtype == "aa":
+                        i.clr = Color
+            if ("-m" in cin) != ("-f" in cin):
                 for i in pops:
                     if i.gender in cin:
                         i.clr = Color
                         verbose(f">{i.Id}: Was painted {Color}")
         except IndexError:
             error("Color must be provided")
+    elif cin[0] == "clear":
+        os.system("clear")
+    elif cin[0] == "pheno":
+        if len(cin) == 1:
+            error("Insufficient args")
+            return
+        if cin[1] == "dom":
+            dominantP = colorize(cin[2])
+        elif cin[1] == "rec":
+            recessiveP = colorize(cin[2])
+        else:
+            error("Second arg must be 'dom' or 'rec'")
     elif cin[0] == "spawn":
         Color = ()
         if "-c" in cin:
@@ -205,6 +248,10 @@ def consoleTwo(cin):
             if Color == -1:
                 return
             cin = cin[:cin.index("-c")]
+        if len(cin) > 1 and cin[1] == "enemy":
+            global noSelection
+            noSelection = False
+            return
         j = 1
         try:
             j = int(cin[1])
@@ -212,6 +259,8 @@ def consoleTwo(cin):
             pass
         for i in range(j):
             pops.add(Pop((random.randint(((SCREEN_W-WIN)/2)-(WIN/4), (SCREEN_W/2)+(WIN/4)), random.randint((SCREEN_H-WIN)/2, (SCREEN_H/2)+(WIN/2))), (0,0,255) if len(Color) != 3 else Color))
+    elif cin[0] == "hwe":
+        print(f">{int(hwe()) == 1}")
     elif cin[0] == "exit":
         running = False
     elif cin[0] == "ls":
@@ -238,16 +287,58 @@ def consoleTwo(cin):
 
 _thread.start_new_thread(console, ())
 
+header = hFont.render(">E-VOLVE :: STATS", None, color['g'])
+sidebar = [3+(SCREEN_W/2)+((3*WIN)/5),(SCREEN_H-WIN_2Y)/2]
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill(color["bk"])
     pygame.draw.rect(screen, color['w'], [((SCREEN_W-WIN)/2)-(WIN/4),(SCREEN_H-WIN)/2,WIN,WIN])
-    pygame.draw.rect(screen, color["gy"], [(SCREEN_W/2)+(WIN/2)+(WIN/10),(SCREEN_H-WIN_2Y)/2,WIN_2X,WIN_2Y])
+    pygame.draw.rect(screen, color["gy"], [sidebar[0]-3,sidebar[1],WIN_2X,WIN_2Y])
     for pop in pops:
+        if not pop.alive:
+            for i in pop.gtype:
+                try:
+                    phenotypes[i].remove(pop.Id)
+                except ValueError:
+                    pass
+            del pop
+            continue
         pop.surf.fill(pop.clr)
         screen.blit(pop.surf, pop.pos)
+        if (pop.Id not in phenotypes['a']) and (pop.Id not in phenotypes['A']):
+            for i in pop.gtype: phenotypes[i].append(pop.Id)
+    try:
+        p = len(phenotypes['a'])/(len(phenotypes['a'])+len(phenotypes['A']))
+        q = len(phenotypes['A'])/(len(phenotypes['a'])+len(phenotypes['A']))
+    except ZeroDivisionError:
+        p = q = 0.0
+    ptxt = font.render(f">>>p = {p}", None, color['g'])
+    qtxt = font.render(f">>>q = {q}", None, color['g'])
+    qsq = font.render(f">>>q² = {q*q}", None, color['g'])
+    psq = font.render(f">>>p² = {p*p}", None, color['g'])
+    tpq = font.render(f">>>2pq = {2*p*q}", None, color['g'])
+    equilibrium = font.render(f">>>p² + 2pq + q² = {hwe()}", None, color['g'])
+    screen.blit(header, (sidebar[0],sidebar[1]+15))
+    screen.blit(font.render(f">>>POP COUNT = {len(pops)}",None,color['g']),(sidebar[0],sidebar[1]+45))
+    screen.blit(ptxt, (sidebar[0],sidebar[1]+75))
+    screen.blit(qtxt, (sidebar[0],sidebar[1]+105))
+    screen.blit(qsq, (sidebar[0],sidebar[1]+135))
+    screen.blit(psq, (sidebar[0],sidebar[1]+165))
+    screen.blit(tpq, (sidebar[0],sidebar[1]+195))
+    screen.blit(equilibrium, (sidebar[0],sidebar[1]+225))
+    screen.blit(font.render(f">>>NO SELECTION == {noSelection}",None,color['g']),(sidebar[0],sidebar[1]+255))
+    screen.blit(font.render(f">>>NO MUTATION == {noMutation}",None,color['g']),(sidebar[0],sidebar[1]+285))
+    screen.blit(font.render(f">>>NO MIGRATION == True",None,color['g']),(sidebar[0],sidebar[1]+315))
+    screen.blit(font.render(f">>>LARGE POPULATION = {len(pops) >= 5}",None,color['g']),(sidebar[0],sidebar[1]+345))
+    screen.blit(font.render(f">>>RANDOM MATING = True",None,color['g']),(sidebar[0],sidebar[1]+375))
+    try:
+        genaverage = sum(genlist)/len(genlist)
+    except ZeroDivisionError:
+        genaverage = 0
+    screen.blit(font.render(f">>>GENERATION AVERAGE: {genaverage}",None,color['g']),(sidebar[0],sidebar[1]+405))
     pygame.display.flip()
 
 pygame.quit()
